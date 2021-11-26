34884c4e275b686fe411c5d95c973b82bea2b167
zhang.lingwei@vpclub.cn <zhang.lingwei@vpclub.cn>
2021-11-26 10:28:15 +0800

    init

--- flink_study_1.13/src/main/java/com/flink/study/Flink01_Source_Collection.java
+++ flink_study_1.13/src/main/java/com/flink/study/Flink01_Source_Collection.java
@@ -1,2 +1,24 @@
-package com.flink.study;public class Flink01_Source_Collection {
+package com.flink.study;
+
+import com.flink.bean.WaterSensor;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Flink01_Source_Collection {
+    public static void main(String[] args) throws Exception {
+        List<WaterSensor> waterSensors = Arrays.asList(
+                new WaterSensor("ws_001", 1577844001L, 45),
+                new WaterSensor("ws_002", 1577844015L, 43),
+                new WaterSensor("ws_003", 1577844020L, 42));
+
+        // 1. 创建执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env
+                .fromCollection(waterSensors)
+                .print();
+        env.execute();
+    }
+
 }
